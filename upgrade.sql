cf24a26af7dfb2e4c7ebd81bbd591240c6b08191
redeyser <redeyser@gmail.com>
2017-06-07 00:01:07 +0300

    fixupgrade

--- upgrade.sql
+++ upgrade.sql
@@ -4,4 +4,4 @@ alter table tb_Zet add column `nf_summa` double(8,2) DEFAULT '0.00';
 alter table tb_Zet add column `nf_discount` double(8,2) DEFAULT '0.00';
 alter table tb_Zet add column `nf_bonus_discount` double(8,2) DEFAULT '0.00';
 alter table tb_Zet add column `nf_vir` double(8,2) DEFAULT '0.00';
-alter table tb_Zet_cont add column `isfiscal`  tinyint(1) default 0;
+alter table tb_Zet_cont add column `isfiscal`  tinyint(1) default 1;
