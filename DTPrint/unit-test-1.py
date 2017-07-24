4455fca5733fe269d181bdb4d79a5d78b4272242
Bloody <Bloody@its66.ru>
2017-07-24 14:40:56 +0500

    Remove python compiled files from repo

--- DTPrint/unit-test-1.py
+++ DTPrint/unit-test-1.py
@@ -6,3 +6,4 @@ import lsusb
 def test_lsserial_1():
     assert sorted(lsusb.lsdir('/dev')) == sorted(lsusb.lsdir_old('/dev'))
     assert sorted(lsusb.lsserial()) == sorted(lsusb.lsserial_old())
+    print(lsusb.lsusb())
